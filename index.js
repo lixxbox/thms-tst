c3e83eb39c24dcb0b331da0bbf594a7eb43d414d
lixxbox <lixxbox@me.com>
2022-07-10 20:17:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let csv = require('csv-parser');
+let fs = require('fs');
+let {render} = require('mustache');
+
+let template = `
+# {{name}}
+{{short_note}}
+Author: [{{author}}](https://www.github.com/{{author}}/) \
+Repo: [{{repo}}](https://www.github.com/{{author}}/{{repo}}/) \
+`;
+
+let output = "";
+
+fs.createReadStream('_data/themes.csv')
+  .pipe(csv({}))
+  .on('data', (row) => {
+    //console.log(row);
+    //console.log("neue zeile")
+    //let output = render(template, row);
+    //fs.writeFileSync("./themes.md", output);
+    output += render(template, row)
+  })
+  .on('end', () => {
+    fs.writeFileSync("./themes.md", output);
+    console.log('CSV file successfully processed');
+  });
+
